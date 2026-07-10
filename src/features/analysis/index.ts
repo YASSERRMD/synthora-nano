503304ad9f1c98c8fe5bceae31ff52759b5fe285
YASSERRMD <arafath.yasser@gmail.com>
2026-07-10 11:50:46 +0400

    feat(analysis): add analysis module exports

---
+++ src/features/analysis/index.ts
@@ -0,0 +1,46 @@
+export type {
+  EvidenceReference,
+  ExtractionResult,
+  AnalysisStage,
+  ProcessingCheckpoint,
+  UserCorrection,
+} from "./types";
+
+export {
+  classifySections,
+  getChunksByCategory,
+  concatenateChunks,
+} from "./section-classifier";
+
+export type { ClassifiedSection } from "./section-classifier";
+
+export {
+  saveCorrections,
+  loadCorrections,
+  applyCorrections,
+  addCorrection,
+  clearCorrections,
+} from "./corrections";
+
+export {
+  saveCheckpoint,
+  loadCheckpoint,
+  clearCheckpoint,
+  createCheckpoint,
+  updateCheckpoint,
+  mergePartialAnalysis,
+} from "./checkpoint";
+
+export { validateCitations, buildEvidenceMap } from "./citation-validator";
+
+export type { CitationValidationResult } from "./citation-validator";
+
+export {
+  createProgress,
+  advanceProgress,
+  buildAnalysisFromCheckpoint,
+  isStageCompleted,
+  getNextIncompleteStage,
+} from "./progress";
+
+export type { AnalysisProgress, AnalysisProgressStage } from "./progress";
